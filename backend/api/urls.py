a95ffa50b13601d3710c8a35bb6e80422b9230ea
Ильгиз Нигматуллин <sleppg@gmail.com>
2023-10-09 19:52:38 +0300

    изменил docker compose и urls api

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -2,12 +2,14 @@ from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
 from api.views import CategoryViewSet, JobViewSet
+from users.views import UserViewSet
 
 app_name = 'api'
 
 router_v1 = DefaultRouter()
 router_v1.register(r'jobs', JobViewSet, basename='jobs')
 router_v1.register(r'category', CategoryViewSet, basename='category')
+router_v1.register(r'users', UserViewSet, basename='users')
 
 urlpatterns = [
     path('', include(router_v1.urls)),
